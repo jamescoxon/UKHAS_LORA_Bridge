85561f27561ab7a41c78f1bea092f2ea90db96ed
jamescoxon <jacoxon@protonmail.com>
2020-09-28 17:34:53 +0100

    added uploading to ukhas.net, also added command arguments: -i <gateway_id> -p <power_output_dbm> to connect to the internet and upload add the argument -c

--- lora.py
+++ lora.py
@@ -1,50 +1,80 @@
 import serial
 import requests
 import time
-
-gateway = 'AB'
-old_line = ""
+import sys, getopt
 
 
-ser = serial.Serial('/dev/ttyACM0', 9600, timeout=2)
-print(ser.name)
+def main(argv):
+    global net_connect
 
-print('Set up radio module')
-set_power = '$P20'
-ser.write(set_power.encode('utf-8'))
-
-while True:
-    line = ser.readline().decode('utf-8').strip()
-
-    if line:
-        if line[0] != '>':
-            print("{} {} ******************".format(time.strftime("<-- %d/%m/%Y %H:%M:%S"), line))
-
-        else:
-            print("{} {}".format(time.strftime("--- %d/%m/%Y %H:%M:%S"), line))
-
-        if "]" in line and line[0] != "[" and line != old_line:
-
-            line_split = line.split("|")
-            if len(line_split) > 1:
-                rx_rssi = line_split[1]
-            else:
-                rx_rssi = "0"
-
-            data = line_split[0]
-#            print("{}: {}".format(time.strftime("%d/%m/%Y %H:%M:%S"), line))
-#            r = requests.post('http://www.ukhas.net/api/upload', json = {'origin': gateway, 'data' : data, 'rssi' : rx_rssi})
-
-    file = open('UKHASnet-decoder/latest.txt')
-    latest = file.readlines()
-    file.close()
     try:
-        if "]" in latest[0] and latest[0][0] != "[" and latest[0] != old_line:
-#    if latest[0] != old_line:
-            print("{} {}".format(time.strftime("--> %d/%m/%Y %H:%M:%S"), latest[0].rstrip()))
-            old_line = latest[0]
-            line_to_send = latest[0]
-            tx_lines = line_to_send.splitlines()
-            ser.write(tx_lines[0].encode('utf-8'))
+        opts, args = getopt.getopt(argv, "i:p:c")
     except:
         print('Error')
+
+    ser = serial.Serial('/dev/ttyACM0', 9600, timeout=2)
+    print(ser.name)
+    gateway = 'AB1'
+    net_connect = 0
+
+    for opt, arg in opts:
+        if opt in ['-i']:
+            gateway = arg
+        elif opt in ['-p']:
+            power = arg
+            print('Set up radio module')
+            set_power = '$P{}'.format(power)
+            ser.write(set_power.encode('utf-8'))
+
+        elif opt in ['-c']:
+            net_connect = 1
+
+    print('ID = {}'.format(gateway))
+
+    old_line = ""
+
+    while True:
+        line = ser.readline().decode('utf-8').strip()
+
+        if line:
+            if line[0] != '>':
+                print("{} {} ***--> ".format(time.strftime("<-- %d/%m/%Y %H:%M:%S"), line), end = '')
+
+
+                if "]" in line and line[0] != "[" and line != old_line:
+
+                    line_split = line.split("|")
+                    if len(line_split) > 1:
+                        rx_rssi = line_split[1]
+                    else:
+                        rx_rssi = "0"
+
+                    data = line_split[0]
+#                print("{}: {}".format(time.strftime("%d/%m/%Y %H:%M:%S"), line))
+                    if net_connect == 1:
+                        r = requests.post('http://www.ukhas.net/api/upload', json = {'origin': gateway, 'data' : data, 'rssi' : rx_rssi})
+                        print("Uploaded")
+                    else:
+                        print('Not Uploaded')
+                else:
+                    print('')
+
+            else:
+                print("{} {}".format(time.strftime("--- %d/%m/%Y %H:%M:%S"), line))
+
+        file = open('UKHASnet-decoder/latest.txt')
+        latest = file.readlines()
+        file.close()
+        try:
+            if "]" in latest[0] and latest[0][0] != "[" and latest[0] != old_line:
+#    if latest[0] != old_line:
+                print("{} {}".format(time.strftime("--> %d/%m/%Y %H:%M:%S"), latest[0].rstrip()))
+                old_line = latest[0]
+                line_to_send = latest[0]
+                tx_lines = line_to_send.splitlines()
+                ser.write(tx_lines[0].encode('utf-8'))
+        except:
+            print('Error')
+
+if __name__ == "__main__":
+    main(sys.argv[1:])

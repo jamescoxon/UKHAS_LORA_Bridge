89a074e1c0a55cf43e010970daa6fd3d6f144bc6
jamescoxon <jacoxon@protonmail.com>
2021-01-18 23:06:17 +0000

    started to add the beginnings of a graphical interface for the chat program

--- chat.py
+++ chat.py
@@ -1,21 +1,79 @@
 import requests
 import time
 import sys
+import curses
 
 import redis
 from redis import Redis
 
 redis_db = redis.Redis(host='localhost', charset="utf-8", decode_responses=True)
 
+
+def shutdown_chat():
+    curses.nocbreak()
+    stdscr.keypad(False)
+    curses.echo()
+    curses.endwin()
+
+def send_text(tx_string):
+
+#    print(tx_string)
+    if len(tx_string) < 64:
+        redis_db.set((int(time.time()) - 1000), tx_string)
+#        print('Loaded into the broadcast queue')
+    else:
+        print('Error string is too long')
+
 id = input('ID: ')
-text = input('Text: ')
 
-tx_string = '6a:{}[{}]'.format(text.replace(' ', '_'), id)
+stdscr = curses.initscr()
+curses.noecho()
 
-print(tx_string)
+max_y, max_x = stdscr.getmaxyx()
+stdscr.border(0)
+stdscr.addstr(4, 5, 'WV Chat System!', curses.A_BOLD)
+stdscr.hline(5, 1, ord('*'), max_x - 2)
+stdscr.addstr(6, 5, 'Press Q (shift q) to close this screen', curses.A_NORMAL)
+stdscr.addstr(10, 5, 'TX LOG: ', curses.A_NORMAL)
+stdscr.addstr(24, 5, 'RX LOG: ', curses.A_NORMAL)
+stdscr.addstr(7, 5, 'Send: ', curses.A_NORMAL)
 
-if len(tx_string) < 64:
-    redis_db.set((int(time.time()) - 1000), tx_string)
-    print('Loaded into the broadcast queue')
-else:
-    print('Error string is too long')
+input_array = ''
+tx_x = 11
+while True:
+    try:
+        ch = stdscr.getch()
+        if ch == ord('Q'):
+            shutdown_chat()
+            sys.exit()
+
+        elif ch == curses.KEY_ENTER or ch == 10:
+            tx_string = '6a:{}[{}]'.format(input_array.replace(' ', '_'), id)
+            send_text(tx_string)
+            stdscr.addstr(tx_x, 5, tx_string, curses.A_NORMAL)
+            input_array = ''
+            tx_x = tx_x + 1
+            if tx_x > 16:
+                tx_x = 11
+
+            stdscr.move(7, 11)
+            stdscr.clrtoeol()
+            stdscr.refresh()
+
+        elif ch == 127:
+            current_y, current_x = stdscr.getyx()
+            if (current_x - 1) >= 11:
+                stdscr.delch(7, current_x - 1)
+#            stdscr.addch(7, current_x - 1, ord(' '))
+#            stdscr.chgat(7, current_x - 1, ord(' '))
+                stdscr.refresh()
+
+        else:
+            if ch > 96 and ch < 123:
+                stdscr.addch(ch)
+                input_array = '{}{}'.format(input_array, chr(ch))
+
+    except KeyboardInterrupt:
+        print('\nExiting')
+        shutdown_chat()
+        sys.exit()
